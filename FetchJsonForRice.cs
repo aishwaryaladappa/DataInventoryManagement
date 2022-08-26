204b85015a4773aa8df65eb5172a0f1fba96a77a
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-26 18:52:54 +0530

    added FetchJsonForRice.cs

---
+++ FetchJsonForRice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInventoryManagement
+{
+    public  class FetchJsonForRice
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+
+
+        }
+    }
+}
