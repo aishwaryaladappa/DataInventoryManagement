b4e7d1c239099d67734881562a73d28397435fe2
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-26 18:12:51 +0530

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DataInventoryManagement
+{
+    class program
+    {
+        public static void Main(string []args)
+        {
+            string path = "C:\\Users\\Admin\\source\\repos\\DataInventoryManagement\\DataInventoryManagement\\Inventory.json";
+            FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+            Rice data = fetchJsonForRice.Read(path);
+            Console.WriteLine("----------------");
+            Console.WriteLine("TypesOfRice");
+            Console.WriteLine("----------------");
+
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typesOfRice.name);
+                Console.WriteLine(data.typesOfRice.Weight);
+                Console.WriteLine(data.typesOfRice.price);
+            }
+        }
+    }
+}
