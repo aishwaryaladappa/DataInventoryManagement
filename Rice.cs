db9fcd6f290d730a317f8798142fcfdd55396105
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-26 18:53:44 +0530

    added Rice.cs

---
+++ Rice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInventoryManagement
+{
+    public class Rice
+    {
+        public TypesOfRice typesOfRice ;
+
+    }
+        public class TypesOfRice
+    {
+        public string name;
+        public string Weight;
+        public string price;
+
+
+
+
+    }
+}
